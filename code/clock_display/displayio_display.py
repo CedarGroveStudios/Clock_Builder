42d944e46adc06fd4c4c87cfaafaf03632b2de0d
CedarGroveStudios <jan@goolsbey.net>
2020-02-17 18:30:37 -0800

    update displayio_display to include dim/restore feature

--- code/clock_display/displayio_display.py
+++ code/clock_display/displayio_display.py
@@ -1,5 +1,5 @@
 # displayio_display.py
-# 2020-02-05 Cedar Grove Studios
+# 2020-02-17 Cedar Grove Studios
 
 import time
 import board
@@ -21,6 +21,7 @@ class DisplayioDisplay:
         self._brightness = brightness
         self._colon      = True
         self._batt_level = 5
+        self._label_restore_color = []  # for restoring text color values
 
         self._weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         self._month   = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
@@ -62,86 +63,7 @@ class DisplayioDisplay:
                                               pixel_shader=self._color_palette,
                                               x=0, y=0)
         self._image_group.append(self._background)
-
-        ### Define labels and values using element grid coordinates
-        # Clock digits grouping
-        self._clock_digits_hour = Label(self._font_1, text="06",
-                                        color=WHITE, max_glyphs=2)
-        self._clock_digits_hour.x = 20
-        self._clock_digits_hour.y = (HEIGHT // 2) + 10
-        self._image_group.append(self._clock_digits_hour)  # image_group[xx]
-
-        self._clock_digits_min = Label(self._font_1, text="23",
-                                       color=WHITE, max_glyphs=2)
-        self._clock_digits_min.x = 74
-        self._clock_digits_min.y = (HEIGHT // 2) + 10
-        self._image_group.append(self._clock_digits_min)  # image_group[xx]
-
-        self._clock_digits_colon = Label(self._font_1, text=":",
-                                         color=WHITE, max_glyphs=1)
-        self._clock_digits_colon.x = 62
-        self._clock_digits_colon.y = (HEIGHT // 2) + 10 - 3
-        self._image_group.append(self._clock_digits_colon)  # image_group[xx]
-
-        # Day Date grouping
-        self._clock_wday = Label(self._font_0, text="Wed",
-                                 color=YELLOW, max_glyphs=3)
-        self._clock_wday.x = 23
-        self._clock_wday.y = 40
-        self._image_group.append(self._clock_wday)  # image_group[xx]
-
-        self._clock_month = Label(self._font_0, text="Feb",
-                                  color=YELLOW, max_glyphs=3)
-        self._clock_month.x = 23 + 35
-        self._clock_month.y = 40
-        self._image_group.append(self._clock_month)  # image_group[xx]
-
-        self._clock_mday = Label(self._font_0, text="05,",
-                                 color=YELLOW, max_glyphs=3)
-        self._clock_mday.x = 23 + 60
-        self._clock_mday.y = 40
-        self._image_group.append(self._clock_mday)  # image_group[xx]
-
-        self._clock_year = Label(self._font_0, text="2020",
-                                 color=YELLOW, max_glyphs=4)
-        self._clock_year.x = 23 + 82
-        self._clock_year.y = 40
-        self._image_group.append(self._clock_year)  # image_group[xx]
-
-        # AM/PM indicator
-        self._clock_ampm = Label(self._font_0, text="PM",
-                                 color=WHITE, max_glyphs=2)
-        self._clock_ampm.x = 120
-        self._clock_ampm.y = (HEIGHT // 2) + 10 - 8
-        self._image_group.append(self._clock_ampm)  # image_group[xx]
-
-        # Time Zone indicator
-        self._clock_dst = Label(self._font_0, text="PST",
-                                color=VIOLET, max_glyphs=3)
-        self._clock_dst.x = 120
-        self._clock_dst.y = (HEIGHT // 2) + 10 + 8
-        self._image_group.append(self._clock_dst)  # image_group[xx]
-
-        # Automatic DST indicator
-        self._clock_auto_dst = Label(self._font_0, text="AutoDST",
-                                     color=VIOLET, max_glyphs=7)
-        self._clock_auto_dst.x = 95
-        self._clock_auto_dst.y = HEIGHT - 8
-        self._image_group.append(self._clock_auto_dst)  # image_group[xx]
-
-        # Alarm indicator
-        self._clock_alarm = Label(self._font_0, text="ALARM",
-                                  color=ORANGE, max_glyphs=5)
-        self._clock_alarm.x = 5
-        self._clock_alarm.y = HEIGHT - 8
-        self._image_group.append(self._clock_alarm)  # image_group[xx]
-
-        # Clock Name indicator
-        self._clock_name = Label(self._font_0, text="Musique_Concrete",
-                                 color=VIOLET, max_glyphs=16)
-        self._clock_name.x = 5
-        self._clock_name.y = 5
-        self._image_group.append(self._clock_name)  # image_group[xx]
+        self._label_restore_color.append(DK_VIO)
 
         # Battery indicator tile grid
         self._sprite_sheet, self._palette = adafruit_imageload.load("/clock_display/batt_sprite_sheet.bmp",
@@ -154,6 +76,99 @@ class DisplayioDisplay:
         self._batt_icon.x = WIDTH - 16
         self._batt_icon.y = 1
         self._image_group.append(self._batt_icon)
+        self._label_restore_color.append(BLACK)
+
+        ### Define labels and values using element grid coordinates
+        # Clock digits grouping
+        self._clock_digits_hour = Label(self._font_1, text="06",
+                                        color=WHITE, max_glyphs=2)
+        self._clock_digits_hour.x = 20
+        self._clock_digits_hour.y = (HEIGHT // 2) + 10
+        self._image_group.append(self._clock_digits_hour)  # image_group[xx]
+        self._label_restore_color.append(WHITE)
+
+        self._clock_digits_min = Label(self._font_1, text="23",
+                                       color=WHITE, max_glyphs=2)
+        self._clock_digits_min.x = 74
+        self._clock_digits_min.y = (HEIGHT // 2) + 10
+        self._image_group.append(self._clock_digits_min)  # image_group[xx]
+        self._label_restore_color.append(WHITE)
+
+        self._clock_digits_colon = Label(self._font_1, text=":",
+                                         color=WHITE, max_glyphs=1)
+        self._clock_digits_colon.x = 62
+        self._clock_digits_colon.y = (HEIGHT // 2) + 10 - 3
+        self._image_group.append(self._clock_digits_colon)  # image_group[xx]
+        self._label_restore_color.append(WHITE)
+
+        # Day Date grouping
+        self._clock_wday = Label(self._font_0, text="Wed",
+                                 color=YELLOW, max_glyphs=3)
+        self._clock_wday.x = 23
+        self._clock_wday.y = 40
+        self._image_group.append(self._clock_wday)  # image_group[xx]
+        self._label_restore_color.append(YELLOW)
+
+        self._clock_month = Label(self._font_0, text="Feb",
+                                  color=YELLOW, max_glyphs=3)
+        self._clock_month.x = 23 + 35
+        self._clock_month.y = 40
+        self._image_group.append(self._clock_month)  # image_group[xx]
+        self._label_restore_color.append(YELLOW)
+
+        self._clock_mday = Label(self._font_0, text="05,",
+                                 color=YELLOW, max_glyphs=3)
+        self._clock_mday.x = 23 + 60
+        self._clock_mday.y = 40
+        self._image_group.append(self._clock_mday)  # image_group[xx]
+        self._label_restore_color.append(YELLOW)
+
+        self._clock_year = Label(self._font_0, text="2020",
+                                 color=YELLOW, max_glyphs=4)
+        self._clock_year.x = 23 + 82
+        self._clock_year.y = 40
+        self._image_group.append(self._clock_year)  # image_group[xx]
+        self._label_restore_color.append(YELLOW)
+
+        # AM/PM indicator
+        self._clock_ampm = Label(self._font_0, text="PM",
+                                 color=WHITE, max_glyphs=2)
+        self._clock_ampm.x = 120
+        self._clock_ampm.y = (HEIGHT // 2) + 10 - 8
+        self._image_group.append(self._clock_ampm)  # image_group[xx]
+        self._label_restore_color.append(WHITE)
+
+        # Time Zone indicator
+        self._clock_dst = Label(self._font_0, text="PST",
+                                color=VIOLET, max_glyphs=3)
+        self._clock_dst.x = 120
+        self._clock_dst.y = (HEIGHT // 2) + 10 + 8
+        self._image_group.append(self._clock_dst)  # image_group[xx]
+        self._label_restore_color.append(VIOLET)
+
+        # Automatic DST indicator
+        self._clock_auto_dst = Label(self._font_0, text="AutoDST",
+                                     color=VIOLET, max_glyphs=7)
+        self._clock_auto_dst.x = 95
+        self._clock_auto_dst.y = HEIGHT - 8
+        self._image_group.append(self._clock_auto_dst)  # image_group[xx]
+        self._label_restore_color.append(VIOLET)
+
+        # Alarm indicator
+        self._clock_alarm = Label(self._font_0, text="ALARM",
+                                  color=ORANGE, max_glyphs=5)
+        self._clock_alarm.x = 5
+        self._clock_alarm.y = HEIGHT - 8
+        self._image_group.append(self._clock_alarm)  # image_group[xx]
+        self._label_restore_color.append(ORANGE)
+
+        # Clock Name indicator
+        self._clock_name = Label(self._font_0, text="Musique_Concrete",
+                                 color=VIOLET, max_glyphs=16)
+        self._clock_name.x = 5
+        self._clock_name.y = 5
+        self._image_group.append(self._clock_name)  # image_group[xx]
+        self._label_restore_color.append(VIOLET)
 
         board.DISPLAY.show(self._image_group)  # Load display
         time.sleep(0.1)  # Allow display to load
@@ -163,7 +178,6 @@ class DisplayioDisplay:
         if self._debug:
             print("*Init:", self.__class__)
             print("*Init: ", self.__dict__)
-
 
     @property
     def zone(self):
@@ -245,15 +259,9 @@ class DisplayioDisplay:
         self._batt_level = level
         self._batt_icon[0] = self._batt_level
 
-    @property
-    def show(self):
-        """Display time via REPL."""
-        return
-
-    @show.setter
     def show(self, datetime):
-        """Display time via REPL."""
-        self._datetime = datetime
+        """Display time via REPL. The primary function of this class."""
+        self._datetime = datetime  # xST structured time object
 
         if self._auto_dst and self._dst:  # changes the text to show DST
             self._clock_dst.text = self._timezone[0] + "DT"
@@ -281,26 +289,28 @@ class DisplayioDisplay:
         else:
             self._clock_alarm.text = "     "
 
-        self._clock_name.text  = "Musique_Concrete"  # add this feature later
+        self._clock_name.text  = "Musique_Concrete"
         self._clock_wday.text  = self._weekday[self._datetime.tm_wday]
         self._clock_month.text = self._month[self._datetime.tm_mon - 1]
         self._clock_mday.text  = "{:02d},".format(self._datetime.tm_mday)
         self._clock_year.text  = "{:04d}".format(self._datetime.tm_year)
 
-        """self._clock_daydate.text = "{} {} {:02d}, {:04d}".format(self._weekday[self._datetime.tm_wday],
-                                                                 self._month[self._datetime.tm_mon - 1],
-                                                                 self._datetime.tm_mday,
-                                                                 self._datetime.tm_year)"""
-        #self._clock_digits.text = "{:02}:{:02}".format(self._hour, self._datetime.tm_min)
         self._clock_digits_hour.text = "{:02}".format(self._hour)
         self._clock_digits_min.text = "{:02}".format(self._datetime.tm_min)
         if self._colon:
             self._clock_digits_colon.text = ":"
         else:
             self._clock_digits_colon.text = ""
+        return
 
-        # Activate display
-        # board.DISPLAY.show(self._image_group)
-        # time.sleep(0.1)  # Allow display to load
+    def dim(self, color=0x444455):
+        """Dim all image group text elements to GRAY."""
+        for i in range(2, len(self._label_restore_color)):
+            self._image_group[i].color = color
+        return
 
+    def restore(self):
+        """Restore all image group text elements to original colors."""
+        for i in range(2, len(self._label_restore_color)):
+            self._image_group[i].color = self._label_restore_color[i]
         return
